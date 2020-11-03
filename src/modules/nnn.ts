1eed495dde440d871862865ff8ccaae56434b94c
Noan <noan.perrot@gmail.com>
2020-11-04 00:34:00 +0100

    fix of the fix

--- src/modules/nnn.ts
+++ src/modules/nnn.ts
@@ -10,15 +10,15 @@ class NNN implements IModule{
         const tar: Date = new Date(2020, 12, 1)
 
         setInterval(() => {
-            
             if(new Date(Date.now()).getHours() === 1 && new Date(Date.now()).getMinutes() === 0){
-            let embed: MessageEmbed = new MessageEmbed()
-            axios.get("https://quotes.rest/qod", {headers: {"Accept": "application/json"}}).then(res => {
-                embed.setTitle(`${31 - new Date(Date.now()).getDate()} jours restants (${Math.round(new Date(Date.now()).getDate() * 100 / 31)}%)`)
-                embed.setDescription(`> ${res.data.contents.quotes[0].quote}\n----------\n*${res.data.contents.quotes[0].author}*`)
-                embed.setColor(0x00ff00);
-                (client.guilds.cache.find(g => g.id === Config.GUILD_ID)?.channels.cache.find(c => c.id === Config.NNN) as TextChannel).send(embed)
-            });
+                let embed: MessageEmbed = new MessageEmbed()
+                axios.get("https://quotes.rest/qod", {headers: {"Accept": "application/json"}}).then(res => {
+                    embed.setTitle(`${31 - new Date(Date.now()).getDate()} jours restants (${Math.round(new Date(Date.now()).getDate() * 100 / 31)}%)`)
+                    embed.setDescription(`> ${res.data.contents.quotes[0].quote}\n----------\n*${res.data.contents.quotes[0].author}*`)
+                    embed.setColor(0x00ff00);
+                    (client.guilds.cache.find(g => g.id === Config.GUILD_ID)?.channels.cache.find(c => c.id === Config.NNN) as TextChannel).send(embed)
+                });
+            }
         }, freq)
     }
 }
