4977a857109575f0c63ee25c2359ae3fb91a066c
Noan <noan.perrot@gmail.com>
2020-11-23 11:48:50 +0100

    update

--- src/modules/autorank.ts
+++ src/modules/autorank.ts
@@ -11,33 +11,35 @@ class AutoRank implements IModule{
 
         setInterval(() => {
             client.guilds.cache.find(g => g.id === Config.GUILD_ID)?.members.fetch({withPresences: true}).then(v => v.forEach(m => {
-                if(m.presence.activities[0] !== undefined){
-                    switch(m.presence.activities[0].type){
-                        case "PLAYING":
-                            if(!m.roles.cache.array().includes(IN_GAME)){
-                                m.roles.add(IN_GAME)
-                            }
-                            break
-                        case "LISTENING":
-                            if(!m.roles.cache.array().includes(MUSIC)){
-                                m.roles.add(MUSIC)
-                            }
-                            break
-                        default:
-                            if(m.roles.cache.array().includes(IN_GAME)){
-                                m.roles.remove(IN_GAME)
-                            }
-                            if(m.roles.cache.array().includes(MUSIC)){
-                                m.roles.remove(MUSIC)
-                            }
-                            break
-                    }
-                }else{
-                    if(m.roles.cache.array().includes(IN_GAME)){
-                        m.roles.remove(IN_GAME)
-                    }
-                    if(m.roles.cache.array().includes(MUSIC)){
-                        m.roles.remove(MUSIC)
+                if(m.presence.status !== "invisible" && m.presence.status !== "offline"){
+                    if(m.presence.activities[0] !== undefined){
+                        switch(m.presence.activities[0].type){
+                            case "PLAYING":
+                                if(!m.roles.cache.array().includes(IN_GAME)){
+                                    m.roles.add(IN_GAME)
+                                }
+                                break
+                            case "LISTENING":
+                                if(!m.roles.cache.array().includes(MUSIC)){
+                                    m.roles.add(MUSIC)
+                                }
+                                break
+                            default:
+                                if(m.roles.cache.array().includes(IN_GAME)){
+                                    m.roles.remove(IN_GAME)
+                                }
+                                if(m.roles.cache.array().includes(MUSIC)){
+                                    m.roles.remove(MUSIC)
+                                }
+                                break
+                        }
+                    }else{
+                        if(m.roles.cache.array().includes(IN_GAME)){
+                            m.roles.remove(IN_GAME)
+                        }
+                        if(m.roles.cache.array().includes(MUSIC)){
+                            m.roles.remove(MUSIC)
+                        }
                     }
                 }
             }))
