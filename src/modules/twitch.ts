14a34596a18c7bdad35ef60cdf9f1f3294e6971b
Noan <noan.perrot@gmail.com>
2020-11-23 11:22:18 +0100

    update

--- src/modules/twitch.ts
+++ src/modules/twitch.ts
@@ -1,6 +1,7 @@
 import { GuildMember, MessageEmbed, TextChannel } from "discord.js";
 import { client } from "../main";
 import { Config } from "../utils/config";
+import { Debug } from "../utils/debug";
 import { IModule } from "./tick";
 
 class Twitch implements IModule{
@@ -11,16 +12,18 @@ class Twitch implements IModule{
             client.guilds.cache.find(g => g.id === Config.GUILD_ID)?.members.cache.forEach(m => {
                 m.presence.activities.forEach(a => {
                     if(a.type === "STREAMING" && !inStream.includes(m as GuildMember)){
-                        const embed: MessageEmbed = new MessageEmbed()
-                        embed.setTitle(`${m.displayName} EST EN LIVE !`)
-                        embed.setDescription(`${a.details}`)
-                        embed.setThumbnail(a.assets?.smallImageURL() || '')
-                        embed.setURL(a.url || '')
                         inStream.push(m as GuildMember);
-                        (m.guild.channels.cache.find(c => c.id === Config.SHARE) as TextChannel).send(embed)
+                        const embed: MessageEmbed = new MessageEmbed();
+                        embed.setTitle(`${m.displayName} EST EN LIVE !`);
+                        embed.setDescription(`${a.details}`);
+                        embed.setThumbnail(a.assets?.smallImageURL() || '');
+                        embed.setURL(a.url || '');
+                        (m.guild.channels.cache.find(c => c.id === Config.SHARE) as TextChannel).send(embed);
+                        Debug.bot(`${m.user.username} added to inStream`);
                     }
                     if(a.type !== "STREAMING" && inStream.includes(m as GuildMember)){
-                        inStream.splice(inStream.indexOf(m as GuildMember), 1)
+                        inStream.splice(inStream.indexOf(m as GuildMember), 1);
+                        Debug.bot(`${m.user.username} removed from inStream`);
                     }
                 })
             })
