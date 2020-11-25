6da426d843a233f54fb858ded4ffdfc284956ff1
Noan <noan.perrot@gmail.com>
2020-11-25 19:56:08 +0100

    Add orthographe

--- src/modules/twitch.ts
+++ src/modules/twitch.ts
@@ -1,4 +1,4 @@
-import { GuildMember, MessageEmbed, TextChannel } from "discord.js";
+import { Activity, GuildMember, MessageEmbed, TextChannel } from "discord.js";
 import { client } from "../main";
 import { Config } from "../utils/config";
 import { Debug } from "../utils/debug";
@@ -10,22 +10,25 @@ class Twitch implements IModule{
         let inStream: Array<GuildMember> = []
         setInterval(() => {
             client.guilds.cache.find(g => g.id === Config.GUILD_ID)?.members.cache.forEach(m => {
-                m.presence.activities.forEach(a => {
-                    if(a.type === "STREAMING" && !inStream.includes(m as GuildMember)){
-                        inStream.push(m as GuildMember);
-                        const embed: MessageEmbed = new MessageEmbed();
-                        embed.setTitle(`${m.displayName} EST EN LIVE !`);
-                        embed.setDescription(`${a.details}`);
-                        embed.setThumbnail(a.assets?.smallImageURL() || '');
-                        embed.setURL(a.url || '');
-                        (m.guild.channels.cache.find(c => c.id === Config.SHARE) as TextChannel).send(embed);
-                        Debug.bot(`${m.user.username} added to inStream`);
+                if(m.presence.status !== "offline" && m.presence.status !== "invisible"){
+                    const a: Activity = m.presence.activities[0]
+                    if(a != undefined){
+                        if(a.type === "STREAMING" && !inStream.includes(m as GuildMember)){
+                            inStream.push(m as GuildMember);
+                            const embed: MessageEmbed = new MessageEmbed();
+                            embed.setTitle(`${m.displayName} EST EN LIVE !`);
+                            embed.setDescription(`${a.details}`);
+                            embed.setThumbnail(a.assets?.smallImageURL() || '');
+                            embed.setURL(a.url || '');
+                            (m.guild.channels.cache.find(c => c.id === Config.SHARE) as TextChannel).send(embed);
+                            Debug.bot(`${m.user.username} added to inStream`);
+                        }
+                        if(a.type !== "STREAMING" && inStream.includes(m as GuildMember)){
+                            inStream.splice(inStream.indexOf(m as GuildMember), 1);
+                            Debug.bot(`${m.user.username} removed from inStream`);
+                        }
                     }
-                    if(a.type !== "STREAMING" && inStream.includes(m as GuildMember)){
-                        inStream.splice(inStream.indexOf(m as GuildMember), 1);
-                        Debug.bot(`${m.user.username} removed from inStream`);
-                    }
-                })
+                }
             })
         }, freq)
     }
