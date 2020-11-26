9eb04ff40405f7acfa92d6fa21d2c3e11bdadba0
Noan <noan.perrot@gmail.com>
2020-11-26 13:58:31 +0100

    update dist

--- dist/commands/goodbye.js
+++ dist/commands/goodbye.js
@@ -6,17 +6,12 @@ var fs_1 = require("fs");
 var debug_1 = require("../utils/debug");
 var Command;
 (function (Command) {
-    //const command = new FfmpegCommand()
     function run(msg, cmd, args) {
         fs_1.readFile("./src/assets/uknowtherule.mp4", function (err, data) {
-            var _a;
             if (err) {
                 debug_1.Debug.bot(err);
                 msg.channel.send("Le bot a givup ! *send uwu in the chat*\n```" + err + "```");
                 return;
-            }
-            if (msg.mentions) {
-                (_a = msg.mentions.users.first()) === null || _a === void 0 ? void 0 : _a.avatarURL({ format: "jpeg" });
             }
             msg.channel.send(new discord_js_1.MessageAttachment(data, "uknowtherule.mp4"));
         });
