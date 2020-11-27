221094e6e498254a7b65dad646a61404a9edd7b2
Noan <noan.perrot@gmail.com>
2020-11-27 14:58:06 +0100

    Rick rolled

---
+++ src/commands/sleep.ts
@@ -0,0 +1,19 @@
+import { Message, MessageAttachment } from "discord.js";
+import { readFile } from "fs";
+import { Command as Cmd } from "../utils/command";
+import { Debug } from "../utils/debug";
+
+namespace Command {
+    export function run(msg: Message, cmd: Cmd.Command, args?: Array<string>){
+        readFile("./src/assets/sleep4ever.mp4", (err, data) => {
+            if(err){
+                Debug.bot(err)
+                msg.channel.send("Le bot n'est pas très funny non plus ! *send uwu in the chat*\n```" + err + "```")
+                return
+            }
+            msg.channel.send(new MessageAttachment(data, "sleep.mp4"))
+        })
+    }
+}
+
+export { Command }
