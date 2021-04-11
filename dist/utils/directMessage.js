12d729139f5e22a32b6870a01791ac7cfe737ffd
Noan <noan.perrot@gmail.com>
2021-04-11 22:12:58 +0200

    Ajout du filtre à injure pour les messages anonymes

--- dist/utils/directMessage.js
+++ dist/utils/directMessage.js
@@ -21,17 +21,29 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DirectMessage = void 0;
 var fs = __importStar(require("fs"));
+var main_1 = require("../main");
 var DirectMessage;
 (function (DirectMessage) {
+    var banwords = new Array();
+    banwords = fs.readFileSync("./banwords.txt").toString().split('\r\n');
     function handle(msg) {
-        console.log(msg.author.username + ": " + msg.content);
-        fs.readFile("./banwords.txt", function (err, data) {
-            console.log(data);
-        });
-        console.log(msg.content.indexOf('pute'));
-        /*(client.guilds.cache.find(g => g.id === config.GUILD_ID)?.channels.cache.find(c => c.id === config.CHANNELS.ANO) as TextChannel).send(
-            `${msg.content}\n==========`
-        )*/
+        var _a;
+        var found = false;
+        var i = 0;
+        while (!found && i < banwords.length) {
+            if (msg.content.indexOf(banwords[i]) != -1) {
+                found = true;
+            }
+            else {
+                i++;
+            }
+        }
+        if (!found) {
+            ((_a = main_1.client.guilds.cache.find(function (g) { return g.id === main_1.config.GUILD_ID; })) === null || _a === void 0 ? void 0 : _a.channels.cache.find(function (c) { return c.id === main_1.config.CHANNELS.ANO; })).send(msg.content + "\n==========");
+        }
+        else {
+            msg.channel.send("Ton message contient du contenu pouvant être offenssant. Il n'a donc pas été envoyé");
+        }
     }
     DirectMessage.handle = handle;
 })(DirectMessage || (DirectMessage = {}));
--- src/utils/directMessage.ts
+++ src/utils/directMessage.ts
@@ -3,15 +3,27 @@ import * as fs from "fs"
 import { client, config } from "../main"
 
 namespace DirectMessage {
+    let banwords: Array<string> = new Array()
+
+    banwords = fs.readFileSync("./banwords.txt").toString().split('\r\n')
+
     export function handle(msg: Message){
-        console.log(`${msg.author.username}: ${msg.content}`)
-        fs.readFile("./banwords.txt", (err, data) => {
-            console.log(data)
-        })
-        console.log(msg.content.indexOf('pute'))
-        /*(client.guilds.cache.find(g => g.id === config.GUILD_ID)?.channels.cache.find(c => c.id === config.CHANNELS.ANO) as TextChannel).send(
-            `${msg.content}\n==========`
-        )*/
+        let found: boolean = false
+        let i: number = 0
+        while(!found && i < banwords.length){
+            if(msg.content.indexOf(banwords[i]) != -1){
+                found = true
+            }else{
+                i++
+            }
+        }
+        if(!found){
+            (client.guilds.cache.find(g => g.id === config.GUILD_ID)?.channels.cache.find(c => c.id === config.CHANNELS.ANO) as TextChannel).send(
+                `${msg.content}\n==========`
+            )
+        }else{
+            msg.channel.send("Ton message contient du contenu pouvant être offenssant. Il n'a donc pas été envoyé")
+        }
     }
 }
 
