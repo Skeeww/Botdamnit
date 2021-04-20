4b4fd0e7a04cb370ae58c1cda0639cdde114b07f
Noan <noan.perrot@gmail.com>
2021-04-20 23:45:18 +0200

    no more dist

--- dist/commands/ping.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.run = void 0;
-function run(cmd) {
-    cmd.msg.content === "ping" ? cmd.msg.channel.send("Pong !") : cmd.msg.channel.send("Ping !");
-}
-exports.run = run;
-//# sourceMappingURL=ping.js.map
--- dist/events/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Reddit_1 = require("./Reddit");
-Object.defineProperty(exports, "Reddit", { enumerable: true, get: function () { return Reddit_1.Reddit; } });
-var Yseult_1 = require("./Yseult");
-Object.defineProperty(exports, "Yseult", { enumerable: true, get: function () { return Yseult_1.Yseult; } });
-var RandomAnswer_1 = require("./RandomAnswer");
-Object.defineProperty(exports, "RandomAnswer", { enumerable: true, get: function () { return RandomAnswer_1.RandomAnswer; } });
-//# sourceMappingURL=index.js.map
