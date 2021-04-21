1d2d3a322b67dbe3ae93abc2c4a1779103d1d5a9
Noan <noan.perrot@gmail.com>
2021-04-21 22:35:36 +0200

    Ajout de SQLite

--- src/events/Suite.ts
+++ src/events/Suite.ts
@@ -1,4 +1,6 @@
-import { client, config } from "../main"
+import { MessageEmbed } from "discord.js"
+import { client, config, db } from "../main"
+import { DatabaseUtil } from "../utils/database"
 import { Debug } from "../utils/debug"
 
 export namespace Suite {
@@ -10,11 +12,26 @@ export namespace Suite {
         if(msg.author.bot || msg.channel.id != config.CHANNELS.RECURENCE){ return }
         const input_number: number = parseInt(msg.content)
         if(input_number !== NaN && input_number === n + 1){
+            if(n == 0){
+                DatabaseUtil.run(db, `INSERT INTO bot(nb_suite) VALUES(${n++})`)
+            }else{
+                DatabaseUtil.run(db, `UPDATE bot SET nb_suite=${n++ + 1} WHERE id=(SELECT MAX(id) FROM bot)`)
+            }
             msg.react('✅')
-            n++
-        }else{
+        }else if(n > 0){
+            const leaderboard: MessageEmbed = new MessageEmbed({
+                title: "Classement des suites",
+                hexColor: "#0099CC"
+            })
+            db.all('SELECT * FROM bot ORDER BY nb_suite DESC', async (err, rows) => {
+                leaderboard.setDescription(`Actuellement, ${rows.length} suites sont enregistrées`)
+                rows.slice(0, 5).forEach(async r => {
+                    leaderboard.addField(`Suite N°${r.id}`, `${r.nb_suite} de longueur`)
+                })
+                await msg.channel.send(leaderboard)
+            })
             msg.react('❌')
-            msg.channel.send(`Série de ${n} !`)
+            msg.channel.send(`Série de ${n}`)
             n = 0
         }
     })
