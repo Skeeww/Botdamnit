7ef23e54d8838d2adb94c2764ab3f78ad63a72ac
Noan <noan.perrot@gmail.com>
2021-05-16 20:23:56 +0200

    Bonjour les renégats !

--- src/commands/renegats.ts
+++ src/commands/renegats.ts
@@ -3,10 +3,10 @@ import * as fs from "fs";
 import { MessageAttachment, ReactionUserManager } from "discord.js";
 
 export function run(cmd: HandledCommand) {
-    fs.readdir(`./src/hidden/`, {}, (err, files) => {
+    fs.readdir(`./src/hidden/renegats`, {}, (err, files) => {
         if(err) throw err
         if(files.length <= 0) ReactionUserManager
-        fs.readFile(`./src/hidden/${files[Math.floor(Math.random() * (files.length))]}`, (err, data) => {
+        fs.readFile(`./src/hidden/renegats/${files[Math.floor(Math.random() * (files.length))]}`, (err, data) => {
             if(err) throw err
             cmd.msg.channel.send(new MessageAttachment(data))
         })
