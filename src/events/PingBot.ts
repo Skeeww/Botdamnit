305ec7d9bea9a0e4b313e18140e262a604cf9fe3
Noan <noan.perrot@gmail.com>
2021-07-29 23:09:47 +0200

    Encore un peu plus d'intelligence

--- src/events/PingBot.ts
+++ src/events/PingBot.ts
@@ -1,18 +1,3 @@
-/*
-curl https://api.openai.com/v1/engines/ada/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-  "prompt": "Salut comment tu vas ?\nBWOAT: Je vais bien.",
-  "temperature": 0,
-  "max_tokens": 128,
-  "top_p": 1,
-  "frequency_penalty": 0,
-  "presence_penalty": 0,
-  "stop": ["\n"]
-}'
-*/
-
 import axios from "axios";
 import { client, config } from "../main";
 import { Debug } from "../utils/debug";
@@ -25,11 +10,11 @@ export namespace PingBot {
     client.on("message", msg => {
         if(!msg.author.bot && msg.mentions.users.has(client.user?.id!)){
             const now = new Date().getTime()
-            if(now - last_command_exec.getTime() > 10000){
+            if(now - last_command_exec.getTime() > 60000){
                 last_command_exec = new Date()
                 
                 const payload = msg.content.replace(/<@(.*?)>/g, "")
-                axios.post("https://api.openai.com/v1/engines/ada/completions", {
+                axios.post("https://api.openai.com/v1/engines/davinci/completions", {
                     "prompt": `${payload}\nBWOAT: `,
                     "temperature": 0,
                     "max_tokens": 80,
@@ -46,7 +31,7 @@ export namespace PingBot {
                     msg.reply(res.data.choices[0].text ?? "je ne sais quoi te répondre face à cela")
                 }).catch(() => msg.reply("Sorry je sleep"))
             }else{
-                msg.reply(`Attends laisse moi ${Math.round((10000 - (now - last_command_exec.getTime())) / 1000)} secondes`)
+                msg.reply(`Attends laisse moi ${Math.round((60000 - (now - last_command_exec.getTime())) / 1000)} secondes`)
             }
         }
     })
