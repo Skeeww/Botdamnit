9b3d68ee5a71b00a47c3e4e3a1155f440ae305d0
Noan <noan.perrot@gmail.com>
2021-07-29 23:10:13 +0200

    Mais moins de bla bla

--- dist/events/PingBot.js
+++ dist/events/PingBot.js
@@ -21,7 +21,7 @@ var PingBot;
                 axios_1.default.post("https://api.openai.com/v1/engines/davinci/completions", {
                     "prompt": payload + "\nBWOAT: ",
                     "temperature": 0,
-                    "max_tokens": 80,
+                    "max_tokens": 60,
                     "top_p": 1,
                     "frequency_penalty": 0,
                     "presence_penalty": 0,
