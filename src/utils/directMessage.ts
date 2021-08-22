b79039356a6f52d9d1413649866b3403669f7f67
Noan <noan.perrot@gmail.com>
2021-08-22 21:24:45 +0200

    goodbye filtre

--- src/utils/directMessage.ts
+++ src/utils/directMessage.ts
@@ -1,29 +1,11 @@
 import { Message, TextChannel } from "discord.js"
-import * as fs from "fs"
 import { client, config } from "../main"
 
 namespace DirectMessage {
-    let banwords: Array<string> = new Array()
-
-    banwords = fs.readFileSync("./banwords.txt").toString().replace('\r', '').split('\n')
-
     export function handle(msg: Message){
-        let found: boolean = false
-        let i: number = 0
-        while(!found && i < banwords.length){
-            if(msg.content.toLowerCase().replace(' ', '').indexOf(banwords[i]) != -1){
-                found = true
-            }else{
-                i++
-            }
-        }
-        if(!found){
-            (client.guilds.cache.find(g => g.id === config.GUILD_ID)?.channels.cache.find(c => c.id === config.CHANNELS.ANO) as TextChannel).send(
-                `${msg.content}\n==========`
-            )
-        }else{
-            msg.channel.send("Ton message contient du contenu pouvant être offenssant. Il n'a donc pas été envoyé")
-        }
+        (client.guilds.cache.find(g => g.id === config.GUILD_ID)?.channels.cache.find(c => c.id === config.CHANNELS.ANO) as TextChannel).send(
+            `${msg.content}\n==========`
+        )
     }
 }
 
