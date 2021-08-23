3deb88acce0c49522909198f8d2ef4dfee04f4cb
Noan <noan.perrot@gmail.com>
2021-08-23 13:42:35 +0200

    Fwurrwy

---
+++ src/commands/uwu.ts
@@ -0,0 +1,117 @@
+import { HandledCommand } from "../utils/commandHandler";
+
+export function run(cmd: HandledCommand) {
+    cmd.msg.delete()
+    if(cmd.args.length <= 0) {
+        cmd.msg.channel.send("YwY youwu nweed to entwer swome twext :3")
+        return
+    }
+    cmd.msg.channel.send(uwuinate(cmd.args))
+}
+
+const kaomojiJoy = [" (* ^ ω ^)", " (o^▽^o)", " (≧◡≦)", " ☆⌒ヽ(*\"､^*)chu", " ( ˘⌣˘)♡(˘⌣˘ )", " xD"]
+const kaomojiEmbarassed = [" (⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)..", " (*^.^*)..,", "..,", ",,,", "... ", ".. ", " mmm..", "O.o"]
+const kaomojiConfuse = [" (o_O)?", " (°ロ°) !?", " (ーー;)?", " owo?"]
+const kaomojiSparkles = [" *:･ﾟ✧*:･ﾟ✧ ", " ☆*:・ﾟ ", "〜☆ ", " uguu.., ", "-.-"]
+  
+function uwuinate(textInput: Array<string>): string {
+    let result = ""
+    textInput.forEach((word: string, index: number) => {
+        result += uwuWord(word.toLowerCase(), index)
+    })
+    return result
+}
+  
+function uwuWord(word: string, index: number): string {
+    let uwu = ""
+    let lastChar = word[word.length - 1]
+    let end = ""
+    let random = 0
+    if (lastChar == '.' || lastChar == '?' || lastChar == '!' || lastChar == ',') {
+      word = word.slice(0, -1)
+      end = lastChar
+      if (end == '.') {
+        random = Math.floor(Math.random() * 3)
+        if (random == 0) {
+          random = Math.floor(Math.random() * kaomojiJoy.length)
+          end = kaomojiJoy[random]
+        }
+      }
+      else if (end == '?') {
+        random = Math.floor(Math.random() * 2)
+        if (random == 0) {
+          random = Math.floor(Math.random() * kaomojiConfuse.length)
+          end = kaomojiConfuse[random]
+        }
+      }
+      else if (end == '!') {
+        random = Math.floor(Math.random() * 2)
+        if (random == 0) {
+          random = Math.floor(Math.random() * kaomojiJoy.length)
+          end = kaomojiJoy[random]
+        }
+      }
+      else if (end == ',') {
+        random = Math.floor(Math.random() * 3)
+        if (random == 0) {
+          random = Math.floor(Math.random() * kaomojiEmbarassed.length)
+          end = kaomojiEmbarassed[random]
+        }
+      }
+  
+      random = Math.floor(Math.random() * 4)
+      if (random == 0) {
+        random = Math.floor(Math.random() * kaomojiSparkles.length)
+        end = kaomojiSparkles[random]
+      }
+    }
+  
+    if (word.indexOf('l') > -1) {
+      if (word.slice(-2) == "le" || word.slice(-2) == "ll") {
+        uwu += word.slice(0, -2).replace(/l/g, 'w').replace(/r/g, 'w') + word.slice(-2) + end + ' '
+      }
+      else if (word.slice(-3) == "les" || word.slice(-3) == "lls") {
+        uwu += word.slice(0, -3).replace(/l/g, 'w').replace(/r/g, 'w') + word.slice(-3) + end + ' '
+      }
+      else {
+        uwu += word.replace(/l/g, 'w').replace(/r/g, 'w') + end + ' '
+      }
+    }
+    else if (word.indexOf('r') > -1) {
+      if (word.slice(-2) == "er" || word.slice(-2) == "re") {
+        uwu += word.slice(0, -2).replace(/r/g, 'w') + word.slice(-2) + end + ' '
+      }
+      else if (word.slice(-3) == "ers" || word.slice(-3) == "res") {
+        uwu += word.slice(0, -3).replace(/r/g, 'w') + word.slice(-3) + end + ' '
+      }
+      else {
+        uwu += word.replace(/r/g, 'w') + end + ' '
+      }
+    }
+    else {
+      uwu += word + end + ' '
+    }
+  
+    uwu = uwu.replace(/you're/g, "ur")
+    uwu = uwu.replace(/youre/g, "ur")
+    uwu = uwu.replace(/fuck/g, "fwickk")
+    uwu = uwu.replace(/shit/g, "poopoo")
+    uwu = uwu.replace(/bitch/g, "meanie")
+    uwu = uwu.replace(/asshole/g, "b-butthole")
+    uwu = uwu.replace(/dick/g, "peenie")
+    uwu = uwu.replace(/penis/g, "peenie")
+    uwu = uwu.replace(/\bcum\b/g, "cummies")
+    uwu = uwu.replace(/\bsemen\b/g, " cummies ")
+    uwu = uwu.replace(/\bass\b/g, " boi pussy ")
+    uwu = uwu.replace(/\bdad\b/g, "daddy")
+    uwu = uwu.replace(/\bfather\b/g, "daddy")
+  
+    if (uwu.length > 2 && uwu[0].match(/[a-z]/i)) {
+      random = Math.floor(Math.random() * 6)
+      if (random == 0) {
+        uwu = uwu[0] + '-' + uwu
+      }
+    }
+
+    return uwu
+}
