b99b68841b6c1429f2d62edce918f0a1f6ac4788
Noan Perrot <noan.perrot@gmail.com>
2021-08-30 11:30:41 +0200

    Time to drake

--- src/utils/database.ts
+++
@@ -1,9 +0,0 @@
-import { Database } from "sqlite3";
-
-export class DatabaseUtil {
-    public static run(db: Database, req: string): void{
-        db.serialize(() => {
-            db.exec(req)
-        })
-    }
-}
